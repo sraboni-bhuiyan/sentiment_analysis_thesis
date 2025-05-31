784d01ea8a7cdfd9ad1e9db2c0432796bdcbcf82
Sraboni Bhuiyan <35885263+sraboni-bhuiyan@users.noreply.github.com>
2025-05-31 19:23:46 +0200

    Delete test_run.py

--- test_run.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import re
-import emoji
-from sklearn.model_selection import train_test_split
-from collections import Counter
-
-df = pd.read_csv('data/amazon_reviews_large.csv')
-print(df.columns.tolist())
